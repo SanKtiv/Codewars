eb29364bde2d55b16e43fac4c3d11d25bf363b12
SanKtiv <aktitorov@gmail.com>
2023-08-10 08:49:12 +0300

    What-dominates-your-array

---
+++ 7kyu_Kata/What-dominates-your-array.js
@@ -0,0 +1,35 @@
+//DESCRIPTION:
+//A zero-indexed array arr consisting of n integers is given. The dominator of array arr is
+// the value that occurs in more than half of the elements of arr.
+// For example, consider array arr such that arr = [3,4,3,2,3,1,3,3]
+// The dominator of arr is 3 because it occurs in 5 out of 8 elements of arr and 5 is more
+// than a half of 8.
+// Write a function dominator(arr) that, given a zero-indexed array arr consisting of n integers,
+// returns the dominator of arr. The function should return −1 if array does not have a dominator.
+// All values in arr will be >=0.
+
+//SOLUTION:
+function dominator (arr) {
+    let n;
+    let z = 0;
+    let x = -1;
+    arr.sort();
+    for (let i = 0; i < arr.length; i++) {
+        n = 0;
+        if (x != arr[i]) {
+            for (let j = i; j <arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    n++;
+                    if (n > arr.length / 2) {
+                        return arr[i];
+                    }
+                }
+            }
+            z += n;
+            if (z >= arr.length / 2) {
+                return -1;
+            }
+            x = arr[i];
+        }
+    }
+}
